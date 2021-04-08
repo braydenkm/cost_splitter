25c8759998550618ffb7c217e4db15b273ca2bbf
Brayden Martin <braydenkaidmartin@gmail.com>
2021-04-08 14:25:43 -0600

    Renamed class folder

--- source/cost_splitter.py
+++ source/cost_splitter.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(__file__), "classes"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "class"))
 sys.path.append(os.path.join(os.path.dirname(__file__), "utility"))
 
 import Group
